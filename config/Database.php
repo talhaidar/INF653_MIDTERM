3f9232042309301f6af52492ab68686c2c4fecbf
talhaidar <70137728+talhaidar@users.noreply.github.com>
2023-03-20 21:43:24 -0400

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -4,7 +4,7 @@
         // DB Params
         private $host;
         private $port;
-        private $db_name;
+        private $dbname;
         private $username;
         private $password;
         private $conn;
@@ -26,11 +26,12 @@
             }
             else{
 
-                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
+                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
                 
                 try {
                     $this->conn = new PDO($dsn, $this->username, $this->password);
                     $this->conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return $this->conn;
                 } catch(PDOException $e) {
                     // echo for tutorial but log the error for production
                     echo 'Connection Error: ' . $e->getMessage();
